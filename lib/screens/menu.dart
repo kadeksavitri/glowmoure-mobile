1a8135d322764965db81c9081aeba5b3a4108c68
kadeksavitri <kadeksavitri5@gmail.com>
2024-11-13 10:10:58 +0700

    1/2 tugas 8

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,38 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:glowmoure_mobile/widgets/left_drawer.dart';
+import 'package:glowmoure_mobile/screens/productentry_form.dart';
+import 'package:glowmoure_mobile/widgets/product_card.dart';
+
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
-    final String npm = '2306203236'; 
-    final String name = 'Kadek Savitri'; 
-    final String className = 'PBP A';
+  final String npm = '2306203236'; // NPM
+  final String name = 'Kadek Savitri'; // Nama
+  final String className = 'PBP A'; // Kelas
+
 
     final List<ItemHomepage> items = [
     ItemHomepage("Lihat Daftar Produk", Icons.shop,Colors.brown),
     ItemHomepage("Tambah Produk", Icons.add, const Color.fromARGB(255, 185, 119, 61)),
     ItemHomepage("Logout", Icons.logout, const Color.fromARGB(255, 190, 162, 110)),
      ];
-  
+
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text(
-        'Glowmoure',
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
+    // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
+    return Scaffold(
+      // AppBar adalah bagian atas halaman yang menampilkan judul.
+      appBar: AppBar(
+        // Judul aplikasi "Mental Health Tracker" dengan teks putih dan tebal.
+        title: const Text(
+          'Glowmoure',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+
         ),
+        // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        // warna icon drawer jadi putih
+        iconTheme: const IconThemeData(color: Colors.white),
+
       ),
-      backgroundColor: Theme.of(context).colorScheme.primary,
-    ),
-    backgroundColor: Theme.of(context).colorScheme.surface,
-    // Bungkus Padding dengan SingleChildScrollView
-    body: SingleChildScrollView(
-      child: Padding(
+      // Masukkan drawer sebagai parameter nilai drawer dari widget Scaffold
+      drawer: const LeftDrawer(),
+      // Body halaman dengan padding di sekelilingnya.
+      body: Padding(
         padding: const EdgeInsets.all(16.0),
+        // Menyusun widget secara vertikal dalam sebuah kolom.
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            // Row untuk menampilkan 3 InfoCard secara horizontal.
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -41,28 +56,39 @@ class MyHomePage extends StatelessWidget {
                 InfoCard(title: 'Class', content: className),
               ],
             ),
+
+            // Memberikan jarak vertikal 16 unit.
             const SizedBox(height: 16.0),
+
+            // Menempatkan widget berikutnya di tengah halaman.
             Center(
               child: Column(
+                // Menyusun teks dan grid item secara vertikal.
+
                 children: [
+                  // Menampilkan teks sambutan dengan gaya tebal dan ukuran 18.
                   const Padding(
                     padding: EdgeInsets.only(top: 16.0),
                     child: Text(
-                      'Welcome to Glowmoure!',
+                      'Welcome to Glowmoure',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 18.0,
                       ),
                     ),
                   ),
+
+                  // Grid untuk menampilkan ItemCard dalam bentuk grid 3 kolom.
                   GridView.count(
                     primary: true,
                     padding: const EdgeInsets.all(20),
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
                     crossAxisCount: 3,
+                    // Agar grid menyesuaikan tinggi kontennya.
                     shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(), 
+
+                    // Menampilkan ItemCard untuk setiap item dalam list items.
                     children: items.map((ItemHomepage item) {
                       return ItemCard(item);
                     }).toList(),
@@ -73,25 +99,28 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
 
 class InfoCard extends StatelessWidget {
+  // Kartu informasi yang menampilkan title dan content.
 
-  final String title;  
-  final String content;  
+  final String title;  // Judul kartu.
+  final String content;  // Isi kartu.
 
   const InfoCard({super.key, required this.title, required this.content});
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      // Membuat kotak kartu dengan bayangan dibawahnya.
       elevation: 2.0,
       child: Container(
-        width: MediaQuery.of(context).size.width / 3.5, 
+        // Mengatur ukuran dan jarak di dalam kartu.
+        width: MediaQuery.of(context).size.width / 3.5, // menyesuaikan dengan lebar device yang digunakan.
         padding: const EdgeInsets.all(16.0),
+        // Menyusun title dan content secara vertikal.
         child: Column(
           children: [
             Text(
@@ -107,58 +136,4 @@ class InfoCard extends StatelessWidget {
   }
 }
 
- class ItemHomepage {
-     final String name;
-     final IconData icon;
-     final Color color;
-
-     ItemHomepage(this.name, this.icon, this.color);
- }
-
- class ItemCard extends StatelessWidget {
-
-  final ItemHomepage item; 
-  
-  const ItemCard(this.item, {super.key}); 
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: item.color,
-      borderRadius: BorderRadius.circular(12),
-      
-      child: InkWell(
-        onTap: () {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(
-              SnackBar(content: Text("Kamu telah menekan tombol ${item.name}!"))
-            );
-        },
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
+ 
